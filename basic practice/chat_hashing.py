eb51cb558c4e20f532e1055bfc52eeb21b5887fb
Avinash Kumar Sharma <134773362+avinash-sharma-121@users.noreply.github.com>
2026-02-06 19:52:30 +0530

    Implement character counting with hash list and dict
    Added functionality to count character occurrences using both a list and a dictionary.

--- basic practice/chat_hashing.py
+++ basic practice/chat_hashing.py
@@ -1,12 +1,37 @@
+freq=["a","b","c","a","b","c","m","c","d","z","z","y","z"]
 
-a=['a','b','c']
+que=["q","b","a","r","c","z"]
 
-print(a)
-for i in a:
-    print(ord(i))
+#lets try with hash_list
 
-#how to get assici value of a character
-print(ord('a'))
-print(ord('b'))
-print(ord('z'))
+hash_list=[0]*26
 
+for i in range(len(freq)):
+    assici_val=ord(freq[i])
+    #print(assici_val)
+    index_value=assici_val-97
+    hash_list[index_value]+=1
+
+#print(hash_list)
+    
+for i in range(len(que)):
+    assici_val=ord(que[i])
+    index_value=assici_val-97
+    print(f"for {que[i]} total char count is {hash_list[index_value]}")
+    
+#Lets try with dict
+
+hash_dict={}
+
+for i  in freq:
+    if i in hash_dict:
+        hash_dict[i]+=1
+    else:
+        hash_dict[i]=1
+print(hash_dict)
+
+for i in que:
+    if i in hash_dict:
+        print(f"for {i} total char count is {hash_dict[i]}")
+    else:
+        print(f"for {i} total char count is 0")
