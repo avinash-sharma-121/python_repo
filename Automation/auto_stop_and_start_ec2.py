147339105adabd3a8e0d82fbd7285d1398df870d
avinash-sharma-121 <kumaravinashsharma81@gmail.com>
2026-02-28 22:13:48 +0530

    created auto_stop_and_start_ec2_script

--- Automation/auto_stop_and_start_ec2.py
+++ Automation/auto_stop_and_start_ec2.py
@@ -2,16 +2,46 @@ import boto3
 
 client=boto3.client("ec2")
 
-#print(client)
+instace_list=[]
 
-ec2_Reservations=client.describe_instances()
-#print(ec2_instance)
+def getInstanceDetails():
 
-ec2_instances=ec2_Reservations['Reservations']
+    ec2_Reservations=client.describe_instances()
+    all_ec2_instances=ec2_Reservations['Reservations']
 
-print(ec2_instances)
-for ec2 in range(len(ec2_instances)):
-    print(ec2)
-    print()
+    if len(all_ec2_instances) > 0:
+        ec2_details=all_ec2_instances[0]['Instances']
+        for ec2 in ec2_details:
+            #print(ec2)
+            instace_id=ec2['InstanceId']
+            #public_ip1=ec2['PublicIpAddress']
+            private_ip1=ec2['PrivateIpAddress']
+            instance_type=ec2['InstanceType']
+            instance_status=ec2['State']['Name']
+            instace_list.append(instace_id)
+            #print(instace_id,public_ip1,private_ip1,instance_type,instance_status)
 
+#getInstanceDetails()
+    
+def stopInstance():
+    getInstanceDetails()
+    print(instace_list)
+    print(type(instace_list[0]))
+    response = client.stop_instances(
+        InstanceIds=instace_list,
+        SkipOsShutdown=True,
+        Force=True
+    )
 
+    print(response)
+
+def startInstance():
+    getInstanceDetails()
+    print(instace_list)
+    response=client.start_instances(
+        InstanceIds=instace_list
+    )
+    print(response)
+
+stopInstance()
+#startInstance()
